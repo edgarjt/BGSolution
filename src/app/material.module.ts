80405987114c3c98bfda92a7e1bc98700628d313
Edgar <edgarjt97@gmail.com>
2020-05-02 03:09:34 -0500

    create module material

---
+++ src/app/material.module.ts
@@ -0,0 +1,14 @@
+import {NgModule} from '@angular/core';
+import {
+  MatCheckboxModule
+} from '@angular/material';
+
+@NgModule({
+  imports: [
+    MatCheckboxModule
+  ],
+  exports: [
+    MatCheckboxModule
+  ]
+})
+export class MaterialModule {}
